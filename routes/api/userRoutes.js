768ec0c6568517f15d44db07a109dc34f81b31b2
Analise <analise.giobbi@gc.com>
2023-03-21 10:37:39 -0400

    starter work for social media api

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// const {
+//     'your controllers here'
+// }
+
+// router.route('/').get('').post('')
+
+// router.route('/:userId').get('')
+
+module.exports = router;
